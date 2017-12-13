be5917252d2a8c2625483d42dbda0874d43e391a
Omid Rafiee <Omid.green.b13@gmail.com>
2017-12-13 19:02:50 +0330

    Add project files.

---
+++ Amlak/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Amlak.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
