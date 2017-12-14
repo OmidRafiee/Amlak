9f0a9db69ba8bb6adcafe767434d8e05d6b2426c
Omid Rafiee <Omid.green.b13@gmail.com>
2017-12-14 14:59:15 +0330

    Create Crud House

--- src/Amlak.Data/Repository/HouseRepository.cs
+++ src/Amlak.Data/Repository/HouseRepository.cs
@@ -1,11 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Alamut.Data.Structure;
+using Amlak.Core.Entities;
+using Amlak.Core.ViewModel;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace Amlak.Data.Repository
 {
     public class HouseRepository
     {
+        private readonly AppDbContext _context;
+
+        public HouseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public ServiceResult<int> Create(HouseViewModel model)
+        {
+            var entity = Mapper.Map<House>(model);
+
+            _context.House.Add(entity);
+            _context.SaveChanges();
+            return ServiceResult<int>.Okay(entity.Id);
+        }
+
+
+        public List<HouseViewModel> GetAll()
+        {
+            return _context.House.ProjectTo<HouseViewModel>().ToList();
+        }
+
+        public ServiceResult Update(HouseViewModel model)
+        {
+            var oldEntity = _context.House.FirstOrDefault(q => q.Id == model.Id);
+            Mapper.Map(model, oldEntity);
+            _context.SaveChanges();
+            var result = ServiceResult<int>.Okay(model.Id);
+            return result;
+        }
+
+        public ServiceResult Delete(int id)
+        {
+            var model = _context.House.Find(id);
+
+            _context.House.Remove(model);
+            _context.SaveChanges();
+            return ServiceResult.Okay("با موفقیت حذف شد");
+        }
 
     }
 }
