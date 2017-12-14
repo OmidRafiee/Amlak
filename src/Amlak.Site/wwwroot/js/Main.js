2fd6cad3e50cbc1b502ad165e7463576b2577cc0
Omid Rafiee <Omid.green.b13@gmail.com>
2017-12-14 10:26:10 +0330

    Add WWW Rote File

---
+++ src/Amlak.Site/wwwroot/js/Main.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    //page Slider Plugin
+    $('.js_PageMainSlider').slick({
+        rtl: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+    $('.js_MiddelSlider').slick({
+        rtl: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+    });
+    // make header sticky ti the top
+    $(".MainHeader").sticky({ topSpacing: 0 });
+
+    // cut string
+    function CutString(obj, LineShow) {
+        if (obj.length) {
+            obj.trunk8({
+                fill: ' ...',
+                lines: LineShow,
+                side: 'right',
+                tooltip: true
+            });
+        }
+    }
+
+    CutString($('.CutString1'), 2);
+    CutString($('.CutString2'), 3);
+    CutString($('.CutString3'), 3);
+    CutString($('.CutString4'), 10);
+
+    $(window).resize(function (event) {
+        CutString($('.CutString1'), 2);
+        CutString($('.CutString3'), 3);
+        CutString($('.CutString4'), 10);
+    });
+});
