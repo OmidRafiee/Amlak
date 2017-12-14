93e627190b607a26e36dae770bf09a66d0fc3b1e
Omid Rafiee <Omid.green.b13@gmail.com>
2017-12-14 12:07:53 +0330

    Create User Class

---
+++ src/Amlak.Core/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Amlak.Core.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        public override int Id { get; set; }
+
+
+        public virtual ICollection<IdentityUserRole<int>> Roles { get; } = new List<IdentityUserRole<int>>();
+
+        public virtual ICollection<IdentityUserClaim<int>> Claims { get; } = new List<IdentityUserClaim<int>>();
+
+        public virtual ICollection<IdentityUserLogin<int>> Logins { get; } = new List<IdentityUserLogin<int>>();
+    }
+}
