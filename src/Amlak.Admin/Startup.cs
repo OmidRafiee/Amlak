45a9469090e488d476971f886e587f18042d1389
Omid Rafiee <Omid.green.b13@gmail.com>
2017-12-19 09:32:29 +0330

    Inital StartUp File

--- src/Amlak.Admin/Startup.cs
+++ src/Amlak.Admin/Startup.cs
@@ -1,32 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using System.Threading.Tasks;
+using Alamut.Utilities.AspNet.Configuration;
+using Alamut.Utilities.Http;
+using Amlak.Core.Entities;
+using Amlak.Core.SSOT;
+using Amlak.Data;
+using Amlak.Data.Repository;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Filters;
 
 namespace Amlak.Admin
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
@@ -50,6 +54,75 @@ namespace Amlak.Admin
                     .WriteTo.RollingFile("logs\\EF-{Date}.log"))
                 .WriteTo.RollingFile("logs\\{Date}.log", restrictedToMinimumLevel: LogEventLevel.Warning)
                 .CreateLogger();
+        }
+
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.ConfigurePoco<FileConfig>(Configuration.GetSection("FileRepository"));
+            //services.ConfigurePoco<AppSetting>(Configuration.GetSection("AppSetting"));
+            //services.ConfigurePoco<SmsSetting>(Configuration.GetSection("SmsSetting"));
+
+
+            services.AddSingleton<HtmlEncoder>(
+                HtmlEncoder.Create(allowedRanges: new[]
+                {
+                    UnicodeRanges.BasicLatin,
+                    UnicodeRanges.Arabic
+                }));
+
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            services.AddScoped<IUserResolverService, UserResolverServiceByHttpContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddScoped<IDbConnection>(options => new SqlConnection(Configuration.GetConnectionString("Default")));
+
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services
+                .AddIdentity<User, Role>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredLength = 6;
+
+                    //options.Cookies.ApplicationCookie.AutomaticAuthenticate = true;
+                    //options.Cookies.ApplicationCookie.AutomaticChallenge = true;
+                    //options.Cookies.ApplicationCookie.LoginPath = "/Account/Login";
+                    //options.Cookies.ApplicationCookie.CookieName = ".Calabin.Admin";
+                })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options => options.LoginPath = "/Account/LogIn");
+
+            services.Configure<RouteOptions>(options =>
+            {
+                options.AppendTrailingSlash = true;
+                options.LowercaseUrls = true;
+            });
+
+            services.AddScoped<HouseRepository>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
 
             if (env.IsDevelopment())
             {
