2ab47b8147b2c220efbe735bf613f7c8bbad308b
Omid Rafiee <Omid.green.b13@gmail.com>
2017-12-23 18:25:13 +0330

    Create House data Entry And Fixed Style House

---
+++ src/Amlak.Core/ViewModel/House/CreateHouseViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Amlak.Core.ViewModel.House
+{
+    public class CreateHouseViewModel
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public string Status { get; set; }
+
+        public long Price { get; set; }
+
+        public int Rooms { get; set; }
+
+        public int Bathrooms { get; set; }
+
+        public int Parkings { get; set; }
+
+        public string PossibilitiesIdsJson { get; set; }
+
+        public string Loacation { get; set; }
+
+
+        public string PhotoGalleryJson { get; set; }
+
+        public string Scale { get; set; }
+
+        public int? CategoryId { get; set; }
+        public int? UserId { get; set; }
+    }
+}
--- src/Amlak.Site/Controllers/HouseController.cs
+++ src/Amlak.Site/Controllers/HouseController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amlak.Core.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Amlak.Site.Controllers
@@ -10,6 +11,7 @@ namespace Amlak.Site.Controllers
     {
         public IActionResult Index()
         {
+            var model = new HouseViewModel();
             return View();
         }
     }
