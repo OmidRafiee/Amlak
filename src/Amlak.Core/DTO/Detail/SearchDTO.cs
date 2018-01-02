1b8589fa07806f0ec68b1eb99a11fa53b2894b9d
Omid Rafiee <Omid.green.b13@gmail.com>
2018-01-02 18:55:56 +0330

    Create Search Dto

---
+++ src/Amlak.Core/DTO/Detail/SearchDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amlak.Core.DTO.Detail
+{
+   public class SearchDTO
+    {
+        public string Town { get; set; }
+        public string City { get; set; }
+        public int Region { get; set; }
+        public string Area { get; set; }
+        public int? CategoryId { get; set; }
+        public int MinScale { get; set; }
+        public int MaxScale { get; set; }
+        public long MinPrice1 { get; set; }
+        public long MinPrice2 { get; set; }
+        public long MinPrice { get; set; }
+        public long MaxPrice { get; set; }
+
+        public List<string> OptionIds { get; set; }
+
+
+    }
+}
