f216c3e23f0fca4234b608dc89333c22d8779730
Omid Rafiee <Omid.green.b13@gmail.com>
2018-01-14 16:59:43 +0330

    Add DropDown list to Edit House

--- src/Amlak.Core/DTO/House/HouseFullDTO.cs
+++ src/Amlak.Core/DTO/House/HouseFullDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using Amlak.Core.Entities;
 
@@ -7,106 +8,112 @@ namespace Amlak.Core.DTO.House
 {
     public class HouseFullDTO
     {
-        public int Id { get; set; }
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "عنوان ملک را وارد نمایید", AllowEmptyStrings = false)]
         public string Title { get; set; }
 
+        [Display(Name = "توضیحات")]
         public string Description { get; set; }
 
         /// <summary>
         /// وضیعت
         /// </summary>
+        [Display(Name = "وضیعت")]
         public string Status { get; set; }
 
         /// <summary>
         /// قیمت
         /// </summary>
+        [Display(Name = "قیمت")]
         public long Price { get; set; }
 
         /// <summary>
-        /// تعداد اطاق
+        /// تعداد اتاق
         /// </summary>
+        [Display(Name = "تعدا اتاق")]
         public int Rooms { get; set; }
 
         /// <summary>
         /// تعداد حمام
         /// </summary>
+        [Display(Name = "تعداد حمام")]
         public int Bathrooms { get; set; }
 
         /// <summary>
         /// تعداد پارکینگ
         /// </summary>
+        [Display(Name = "تعداد پارکینگ")]
         public int Parkings { get; set; }
 
         /// <summary>
         /// امکانات که برای مثال شامل
         ///سیستم گرمایشی ، کولر 
         /// </summary>
+        [Display(Name = "امکانات")]
         public string OptionIdsJson { get; set; }
 
         /// <summary>
         /// مکان جغرافیای
         /// </summary>
+        [Display(Name = "مکان جغرافیایی")]
         public string Loacation { get; set; }
 
         /// <summary>
         /// تصاویر ملک
         /// </summary>
+        [Display(Name = "تصاویر ملک")]
         public string PhotoGalleryJson { get; set; }
 
         /// <summary>
         /// متراژ ملک
         /// </summary>
-        public string Scale { get; set; }
+        [Display(Name = "متراژ")]
+        public int Scale { get; set; }
 
         /// <summary>
         /// منطفه
         /// </summary>
+        [Display(Name = "منطقه")]
         public string Region { get; set; }
 
         /// <summary>
         /// شهر
         /// </summary>
+        [Display(Name = "شهر")]
         public string City { get; set; }
 
         /// <summary>
         /// استان
         /// </summary>
+        [Display(Name = "استان")]
         public string Town { get; set; }
 
 
         /// <summary>
         /// محله
         /// </summary>
+        [Display(Name = "محله")]
         public string Area { get; set; }
 
         /// <summary>
         /// طبقه 
         ///  </summary>
+        [Display(Name = "طبقه")]
         public int Floor { get; set; }
-
-        /// <summary>
-        /// پیشنهاد ویژه
-        /// </summary>
-        public bool IsSpecialOffer { get; set; }
-
-        /// <summary>
-        /// زمان ثبت آگهی
-        /// </summary>
-        public DateTime RequestDate { get; set; }
-
-        /// <summary>
-        ///زمان انتشار آگاهی که توسط ادمین تهیین میشود 
-        /// </summary>
-        public DateTime? PublishDate { get; set; }
-
-        public bool IsPublished { get; set; }
 
 
         public int? UserId { get; set; }
+
+
+        [Display(Name = "نوع ملک")]
         public int? CategoryId { get; set; }
+        public DateTime RequestDate { get; set; } = DateTime.Now;
 
+        [Display(Name = "نمایش در سایت")]
+        public bool IsPublished { get; set; }
 
-        public virtual Category Category { get; set; }
+        [Display(Name = "نمایش در بخش پیشنهادهای ویژه")]
+        public bool IsSpecialOffer { get; set; }
 
     }
 }
