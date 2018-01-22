e469bd49a3a951373159b3a06e0018c167a28ff6
Omid Rafiee <Omid.green.b13@gmail.com>
2018-01-22 17:36:16 +0330

    Add News Entity

---
+++ src/Amlak.Core/Entities/News.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amlak.Core.Entities
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Body { get; set; }
+        public string Image { get; set; }
+        public string File { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public bool IsPublished { get; set; }
+    }
+}
