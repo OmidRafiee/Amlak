386bc72d417fb18de1fd4470e1f6ac1c8ddd3420
Omid Rafiee <Omid.green.b13@gmail.com>
2018-01-29 12:00:07 +0330

    Remove test Filter Action

--- src/Amlak.Admin/Controllers/HomeController.cs
+++ src/Amlak.Admin/Controllers/HomeController.cs
@@ -15,49 +15,9 @@ namespace Amlak.Admin.Controllers
     {
         public IActionResult Index()
         {
-            var request = Request.HttpContext.Request;
-
-            // دسترسی به شیئ کش
-            //  var cache = context.HttpContext.Cash;
-
-            // کاربر IP بدست آوردن
-            //  var IP = request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? request.UserHostAddress;
-            var IP = request.HttpContext.Connection.RemoteIpAddress;
-
-            // مشخصات مرورگر
-            var browser = request.Headers["User-Agent"].ToString();
-
             return View();
         }
 
-
-
-        [HttpPost]
-        [StopSpamActionFilter(DelayRequest = 0)]
-        public IActionResult Index(int id)
-        {
-            var request = Request.HttpContext.Request;
-
-            // دسترسی به شیئ کش
-            //  var cache = context.HttpContext.Cash;
-
-            // کاربر IP بدست آوردن
-            //  var IP = request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? request.UserHostAddress;
-            var IP = request.HttpContext.Connection.RemoteIpAddress;
-
-            // مشخصات مرورگر
-            var browser = request.Headers["User-Agent"].ToString();
-
-            // در اینجا آدرس درخواست جاری را تعیین می‌کنیم
-          //  var targetInfo = (this.AddAddress) ? (request.GetDisplayUrl() + request.QueryString) : "";
-
-            // شناسه یکتای درخواست
-            //var Uniquely = String.Concat(IP, browser, targetInfo);
-
-
-
-            return RedirectToAction("About");
-        }
 
         public IActionResult About()
         {
